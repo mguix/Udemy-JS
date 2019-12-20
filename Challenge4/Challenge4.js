b3bb3a080a2223252b6ecaaf0940537368d8ea60
Mireia Guix <mguix@users.noreply.github.com>
2019-12-20 18:29:28 +0000

    added exercice 4

--- Challenge4/Challenge4.js
+++ Challenge4/Challenge4.js
@@ -10,3 +10,51 @@ Let's remember the first coding challenge where Mark and John compared their BMI
 Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
 GOOD LUCK 😀
 */
+
+let john = {
+  name: "John Smith",
+  mass: 93,
+  height: 1.89,
+  johnBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+};
+
+let mark = {
+  name: "Mark",
+  mass: 84,
+  height: 1.71,
+  markBMI: function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  }
+};
+
+console.log(john.johnBMI());
+console.log(mark.markBMI());
+console.log(john, mark);
+
+if (john.bmi > mark.bmi) {
+  console.log(
+    john.name +
+      " has a higher BMI than Mark, with a BMI of " +
+      Math.round(john.bmi) +
+      " a mass of " +
+      john.mass +
+      " and a height of " +
+      john.height
+  );
+} else if (john.bmi < mark.bmi) {
+  console.log(
+    mark.name +
+      " has a higher BMI than John, with a BMI of " +
+      mark.bmi +
+      " a mass of " +
+      mark.mass +
+      " and a height of " +
+      mark.height
+  );
+} else {
+  console.log("John and Mark have the same BMI");
+}
